81e0a0859ab3d97febac15115fdf3ab3cac56711
siberry <siberry226@gmail.com>
2019-06-05 16:49:25 -0400

    User model added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_05_150433) do
+ActiveRecord::Schema.define(version: 2019_06_05_204418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 2019_06_05_150433) do
     t.string "img_url"
     t.boolean "change_image"
     t.string "img_source"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
