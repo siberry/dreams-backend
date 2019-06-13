f18e633e1d39917665018cf3b69c672b99d2f5cd
siberry <siberry226@gmail.com>
2019-06-13 15:21:21 -0400

    Follow#destroy

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,7 +1,16 @@
 class FollowsController < ApplicationController
   def create
     follow = Follow.create(params.require(:follow).permit(:user_id, :followed_id))
+    user = User.find(params.require(:user_id))
 
-    render json: follow
+    render json: user
+  end
+
+  def destroy
+    follow = Follow.find(params[:id])
+    user = follow.follower
+    follow.destroy
+
+    render json: user
   end
 end
