e463572606c3e6cd99afb9d9ace690f23712474b
siberry <siberry226@gmail.com>
2019-07-11 13:11:12 -0400

    Credentials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,17 +5,18 @@ class ApplicationController < ActionController::API
 
   def decode_token
     begin
-      JWT.decode(get_auth_header, "hide_this_please")[0]["user_id"]
+      JWT.decode(get_auth_header, Rails.application.credentials.auth_key)[0]["user_id"]
     rescue
       nil
     end
   end
 
   def encode_token(user)
-    JWT.encode({user_id: user.id}, "hide_this_please")
+    JWT.encode({user_id: user.id}, Rails.application.credentials.auth_key)
   end
 
   def session_user
     User.find_by(id: decode_token)
   end
+
 end
